a908504662c87beedcb7f2e30719b04cd0cd1a01
chenbiao51 <2025694053@qq.com>
2022-10-09 23:00:55 +0800

    dev

--- NFComm/NFLubanPlugin/NFLubanModule.cpp
+++ NFComm/NFLubanPlugin/NFLubanModule.cpp
@@ -28,159 +28,48 @@
 
 bool NFLubanModule::Awake()
 {
-	int argc = 0;
-	char* c = new char[1];
-	//::Lubaning::GLuban_FLAG(output) = "xml:hello.xml";
-	//::Lubaning::InitGoogleLuban(&argc, &c);
-
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->Awake();
-	}
-
 	return true;
 }
 
 bool NFLubanModule::Init()
 {
-	//find all plugins and all modules, then check whether they have a Lubaner
-	//if any module have't a Lubaner for it then  can not start the application
-	//this is a rule for NF's world to keep high quality code under TDD
-
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->Init();
-	}
-
+	
     return true;
 }
 
 bool NFLubanModule::AfterInit()
 {
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->AfterInit();
-	}
-
+	
 	return true;
 }
 
 bool NFLubanModule::CheckConfig()
 {
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->CheckConfig();
-	}
-
 	return true;
 }
 
 bool NFLubanModule::ReadyExecute()
 {
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->ReadyExecute();
-	}
-
 	return true;
 }
 
 bool NFLubanModule::Execute()
 {
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->Execute();
-	}
 
     return true;
 }
 
 bool NFLubanModule::BeforeShut()
 {
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->BeforeShut();
-	}
-
 	return true;
 }
 
 bool NFLubanModule::Shut()
 {
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->Shut();
-	}
 
 	return true;
 }
 
-bool NFLubanModule::Finalize()
-{
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->Finalize();
-	}
 
-	return true;
-}
 
-bool NFLubanModule::OnReloadPlugin()
-{
-	std::list<NFIModule*> xModules = pPluginManager->Modules();
-	for (auto it : xModules)
-	{
-		NFIModule* pModule = it;
-		NFIModule* pLubanModule = pPluginManager->FindModule(pModule->name);
-		pLubanModule->OnReloadPlugin();
-	}
 
-	return true;
-}
-
-int NFLubanModule::Factorial(int n)
-{
-	if (1 == n)
-	{
-		return 1;
-	}
-
-	return n*Factorial(n-1);
-}
-
-// 测试用例
-/*
-Luban(FactorialLuban, HandlesPositiveInput) {
-    EXPECT_EQ(1, this->Factorial(1));
-    EXPECT_EQ(2, this->Factorial(2));
-    EXPECT_EQ(6, this->Factorial(3));
-    EXPECT_EQ(40320, this->Factorial(8));
-}
-*/
